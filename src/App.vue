c1908c193f2cace8dc5d113ad1e2fc3b24142c79
Yolit <yolit.zr@gmail.com>
2022-04-13 18:33:30 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <product-list-one></product-list-one>
+    <product-list-two></product-list-two>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      products: [
+        {name: 'Banana Skin', price: 20},
+        {name: 'Shiny Star', price: 40},
+        {name: 'Green Shells', price: 60},
+        {name: 'Red Shells', price: 80},
+        {name: 'Purple Shells', price: 100},
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
